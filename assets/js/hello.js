1007f182c29e0f8e11bc09657a8ddc19462f21a1
llyubn05-collab <llyubn05@snu.ac.kr>
2026-05-03 05:29:12 +0000

    과제09 페이지 완성

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -1,13 +1,11 @@
 //assets/js/hello.js
-const author = "이상";
-const title = "날개";
-const year = 1936;
-const today = 2026;
-const yearsPassed = today - year;
+const author = "임유빈";
+const major = "통계학";
+const startYear = 2026;
+
 
 console.log(
-  `안녕하세요! ${author}의 「${title}」 (${year}) ` +
-  `페이지에 오신 것을 환영합니다.`
+  `안녕하세요! ${author}(${major} 전공)의 페이지에 오신 것을 환영합니다.`
 );
 
-console.log(`이 작품은 발표된 지 ${yearsPassed}년이 되었습니다.`);
+console.log(`이 사이트는 ${startYear}년에 개설되었습니다.`);
