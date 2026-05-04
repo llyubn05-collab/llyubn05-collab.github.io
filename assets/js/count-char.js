3e43534e4cbf146d05f13145cf22fb9184e5cf71
llyubn05-collab <llyubn05@snu.ac.kr>
2026-05-04 07:46:13 +0000

    한글 글자 세기 추가

--- assets/js/count-char.js
+++ assets/js/count-char.js
@@ -10,3 +10,18 @@ for (const ch of text) {
 
 console.log(`"${text}"`);
 console.log(`'${target}' 글자는 ${count}번 등장합니다.`);
+
+let hangul = 0;
+let total = 0; // 전체 텍스트가 몇 글자인가? 
+
+for (const ch of text) {
+    if (ch !== " ") { // 공백은 빼고 세기
+        total++;
+        if (ch >= "가" && ch <= "힣") {
+            hangul++;
+        }
+    }
+}
+
+const ratio = (hangul / total) * 100;
+console.log(`'한글 글자' 비율: ${ratio.toFixed(1)}%`);
