657a4cb19727b7b11866115645b8402f284ce6f9
llyubn05-collab <llyubn05@snu.ac.kr>
2026-05-27 11:19:42 +0000

    과제 12 (1)

--- assets/js/hw12.js
+++ assets/js/hw12.js
@@ -3,6 +3,39 @@
 
 // Q1
 fetch("/data/sillok.json")
+    .then(response => response.json())
+    .then(records => {
+        const labels = records.map(r => r.kings);
+        const counts = records.map(r => r.volumes);
+
+        const canvas = document.querySelector("q1-chart");
+        new Chart(canvas, {
+            type: "bar",
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: "권수",
+                    data: counts,
+                    backgroundColor: "rgba(54, 162, 235, 0.6)"
+                }
+                ]
+            },
+            options: {
+                plugins: {
+                    title: { display: true, text: "조선왕조실록 왕대별 권수"}
+                },
+                scales: {
+                    y: { beginAtZero: true}
+                }
+            }
+        })
+
+    })
+
+
+
+
+
 
 // Q2
-fetch("/data/nobel-literature.csv")
+// fetch("/data/nobel-literature.csv")
